46d3c1a26adb43c41dd8bdb7644906f5be88ff04
Kevin Bastoul <kaycbas@gmail.com>
2020-12-21 16:37:02 -0800

    fixed login form

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -37,8 +37,9 @@ export const signup = user => dispatch => (
 );
 
 // Upon login, set the session token and dispatch the current user. Dispatch errors on failure.
-export const login = user => dispatch => (
-    APIUtil.login(user).then(res => {
+export const login = user => dispatch => {
+    debugger
+    return APIUtil.login(user).then(res => {
         const { token } = res.data;
         localStorage.setItem('jwtToken', token);
         APIUtil.setAuthToken(token);
@@ -48,7 +49,7 @@ export const login = user => dispatch => (
     .catch(err => {
         dispatch(receiveErrors(err.response.data));
     })
-)
+}
 
 // We wrote this one earlier
 export const logout = () => dispatch => {
