8bd4089e2113478666fb1b9f369f9d366e5a20da
Kevin Bastoul <kaycbas@gmail.com>
2020-12-21 16:38:20 -0800

    finish auth

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -38,7 +38,6 @@ export const signup = user => dispatch => (
 
 // Upon login, set the session token and dispatch the current user. Dispatch errors on failure.
 export const login = user => dispatch => {
-    debugger
     return APIUtil.login(user).then(res => {
         const { token } = res.data;
         localStorage.setItem('jwtToken', token);
