5c697b09b6f690264cf9907d229fe1c043e61669
Kevin Bastoul <kaycbas@gmail.com>
2020-12-22 16:16:36 -0800

    add heroku prep

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const port = process.env.PORT || 5000;
 
@@ -14,6 +15,13 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
